4bacf4d6a0f67226fa486880cb9e9d2f109bc54f
Zhansaule Baidalinova <54521128+Zhansaule-it@users.noreply.github.com>
2021-03-20 17:15:17 +0600

    Update my_scarep.py

--- my_scarep.py
+++ my_scarep.py
@@ -1,33 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+
+#  function take a repository array of dict and transform it and return it into a CSV string
 def format(repositories_data):
-    res = "deverloper,repository_name,nbr_stars\n"
+    CSVstring = "deverloper,repository_name,nbr_stars\n"
     for i in range(len(repositories_data)):
-        res +="".join((repositories_data[i]['deverloper'])+","+(repositories_data[i]['repository_name'])+","+(repositories_data[i]['nbr_stars'])+"\n")
-    print(res)
+        CSVstring +="".join((repositories_data[i]['deverloper'])+","+(repositories_data[i]['repository_name'])+","+(repositories_data[i]['nbr_stars'])+"\n")
+    print(CSVstring)
+    return CSVstring
+
+# function take an array of the repository row and return an array of dictionary
 def transform(html_repos):
     res = []
     for i in range(0,len(html_repos)-2,3): 
          res.append({'deverloper': html_repos[i], 'repository_name': html_repos[i+1], 'nbr_stars': html_repos[i+2]})
     format(res)
+    return res
+
+# function extract data about repostories from html code and return it
 def extract(page):
-    from bs4 import BeautifulSoup
-    import requests
     soup = BeautifulSoup(page.text, "lxml")
     name_project= []
     star = []
     art = soup.find_all('article',class_="Box-row")
     for tag in art:
         name_project.append(tag.find_all('h1',class_="h3 lh-condensed")[0].text.strip("\n").strip().split("/"))
-        star.append(tag.find_all('a',class_="muted-link d-inline-block mr-3")[0].text.strip("\n").strip())
+        star.append(tag.find_all('a',class_="Link--muted d-inline-block mr-3")[0].text.strip("\n").strip())
+    # the name of class of tag "a" sometimes changes, previously name was "muted-link d-inline-block mr-3", i hope 
     all_data = []
     for i in range(len(name_project)):
         all_data.append(name_project[i][0].strip())
         all_data.append(name_project[i][1].rstrip().strip("\n").strip())
         all_data.append(star[i])
-    #print(all_data)
     transform(all_data)
+    return all_data
+
+# function get request from giving url and return it
 def request_github_trending(url):
-    import requests
     response = requests.get(url)
     extract(response) 
+    return response
+
 url = 'https://github.com/trending'
 request_github_trending(url)
